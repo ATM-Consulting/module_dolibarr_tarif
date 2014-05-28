4878a3cca2a77138b61b3c17cbdea77d5a001137
Alexis Algoud <alexis@atm-consulting.fr>
2014-05-28 16:09:49 +0200

    debug si multidevise

--- liste.php
+++ liste.php
@@ -252,9 +252,22 @@
 	if($conf->multidevise->enabled){
 
 		$sql = "SELECT tc.rowid AS 'id', tc.tva_tx AS tva, tc.type_price as type_price, c.code as currency,pays.libelle as 'Pays', tc.price_base_type AS base, tc.quantite as quantite,
-					   tc.unite AS unite, tc.remise_percent AS remise, tc.prix AS prix, p.".$type_unite."_units AS base_poids, tc.unite_value AS unite_value,
-					   ((tc.quantite * POWER(10,(tc.unite_value-p.".$type_unite."_units))) * tc.prix) - ((tc.quantite * POWER(10,(tc.unite_value-p.".$type_unite."_units))) * tc.prix) * (tc.remise_percent/100)  AS 'Total',
-					   '' AS 'actions'
+					   tc.unite AS unite, tc.remise_percent AS remise, tc.prix AS prix ";
+		
+		if($type_unite == "unite") {
+
+			$sql.=" tc.unite_value AS unite_value,
+				tc.quantite * tc.prix * (100-tc.remise_percent)/100 AS 'Total'";
+		
+		}
+		else {
+			
+			$sql.=" , p.".$type_unite."_units AS base_poids, tc.unite_value AS unite_value,
+				((tc.quantite * POWER(10,(tc.unite_value-p.".$type_unite."_units))) * tc.prix) - ((tc.quantite * POWER(10,(tc.unite_value-p.".$type_unite."_units))) * tc.prix) * (tc.remise_percent/100)  AS 'Total'";
+			
+		}
+					   
+		$sql.= " , '' AS 'actions'
 				FROM ".MAIN_DB_PREFIX."tarif_conditionnement AS tc
 					LEFT JOIN ".MAIN_DB_PREFIX."product AS p ON (tc.fk_product = p.rowid)
 					LEFT JOIN ".MAIN_DB_PREFIX."currency AS c ON (c.code = tc.currency_code)
