72489b795f61edd77620f98b4c2650a09d06c249
Alexis Algoud <alexis@atm-consulting.fr>
2014-10-23 09:37:38 +0200

    Mise en const du config (pour être en abricot cohérent)

--- config.php
+++ config.php
@@ -1,4 +1,7 @@
 <?php
 	require('default.config.php');
-	
-	define('TARIF_DEFAULT_TYPE', 'PRICE');
+
+	define('FACTURE_DISPATCH_ON_EXPEDITION', $conf->global->TARIF_FACTURE_DISPATCH_ON_EXPEDITION); // TODO global dolibarr 0
+        define('DONT_ADD_UNIT_SELECT', $conf->global->TARIF_DONT_ADD_UNIT_SELECT);	//default 1
+	define('TARIF_DEFAULT_TYPE', $conf->global->TARIF_DEFAULT_TYPE); // PRICE
+        define('DOL_DEFAULT_UNIT', $conf->TARIF_DOL_DEFAULT_UNIT ); //'unite'  //Définie l'unité de vente par défaut des lignes de propale, commande, facture : weight, size, surface, volume
