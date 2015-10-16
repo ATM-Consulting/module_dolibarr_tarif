e5faed3d91e92c4b0bf79a828cc2ee1bf8a3ba66
arnaud <arnaud.saint-patrice@atm-consulting.fr>
2015-10-16 15:50:55 +0200

    FIX define & config default

--- config.php
+++ config.php
@@ -1,8 +1,17 @@
 <?php
-	require('default.config.php');
+	require('config.default.php');
 
-	define('FACTURE_DISPATCH_ON_EXPEDITION', $conf->global->TARIF_FACTURE_DISPATCH_ON_EXPEDITION); // TODO global dolibarr 0
-    define('DONT_ADD_UNIT_SELECT', $conf->global->TARIF_DONT_ADD_UNIT_SELECT);	//default 1
-	define('TARIF_DEFAULT_TYPE', $conf->global->TARIF_DEFAULT_TYPE); // PRICE
-    define('DOL_DEFAULT_UNIT', $conf->global->TARIF_DOL_DEFAULT_UNIT ); //'unite'  //Définie l'unité de vente par défaut des lignes de propale, commande, facture : weight, size, surface, volume
-	define('TARIF_DONT_USE_TVATX', $conf->global->TARIF_DONT_USE_TVATX); // Garder le taux de TVA déterminé par Dolibarr
+	if(!defined('FACTURE_DISPATCH_ON_EXPEDITION'))
+		define('FACTURE_DISPATCH_ON_EXPEDITION', $conf->global->TARIF_FACTURE_DISPATCH_ON_EXPEDITION); // TODO global dolibarr 0
+
+	if(!defined('DONT_ADD_UNIT_SELECT'))
+	    define('DONT_ADD_UNIT_SELECT', $conf->global->TARIF_DONT_ADD_UNIT_SELECT);	//default 1
+
+	if(!defined('TARIF_DEFAULT_TYPE'))
+		define('TARIF_DEFAULT_TYPE', $conf->global->TARIF_DEFAULT_TYPE); // PRICE
+
+	if(!defined('DOL_DEFAULT_UNIT'))
+	    define('DOL_DEFAULT_UNIT', $conf->global->TARIF_DOL_DEFAULT_UNIT ); //'unite'  //Définie l'unité de vente par défaut des lignes de propale, commande, facture : weight, size, surface, volume
+
+	if(!defined('TARIF_DONT_USE_TVATX'))
+		define('TARIF_DONT_USE_TVATX', $conf->global->TARIF_DONT_USE_TVATX); // Garder le taux de TVA déterminé par Dolibarr
